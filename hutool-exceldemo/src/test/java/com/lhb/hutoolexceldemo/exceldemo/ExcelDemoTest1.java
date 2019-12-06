18e6cb17e64757a732b7aadb9f4469c2a3f6f9da
wave.lee <531003231@qq.com>
2019-12-06 17:15:47 +0800

    [FIX]HuTool工具类库大数据excel生成-2

--- hutool-exceldemo/src/test/java/com/lhb/hutoolexceldemo/exceldemo/ExcelDemoTest1.java
+++ hutool-exceldemo/src/test/java/com/lhb/hutoolexceldemo/exceldemo/ExcelDemoTest1.java
@@ -1,13 +1,12 @@
 package com.lhb.hutoolexceldemo.exceldemo;
 
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.poi.excel.BigExcelWriter;
 import cn.hutool.poi.excel.ExcelUtil;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,23 +22,22 @@ public class ExcelDemoTest1 {
         long startTimes = System.currentTimeMillis();
         List<List<?>> list=new ArrayList<>();
         List<?> row=null;
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 10; i++) {
             row = CollUtil.newArrayList("aa"+i, "bb"+i, "cc"+i, "dd"+i,"ee"+i,DateUtil.date(), i);
             list.add(row);
         }
-        List<List<?>> rows = CollUtil.newArrayList(list);
 
         String fileName= String.valueOf(System.currentTimeMillis());
         BigExcelWriter writer= ExcelUtil.getBigWriter("C:/Users/LHB/Desktop/acc/excel/"+fileName+".xlsx");
 
         //跳过第一行
 //        writer.passCurrentRow();
-
         //设置标题，合并首行单元格
         writer.merge(row.size()-1,"这是一个标题");
-// 一次性写出内容，使用默认样式
-        writer.write(rows);
-// 关闭writer，释放内存
+        // 一次性写出内容，使用默认样式
+        writer.write(list);
+        // 关闭writer，释放内存
+
         writer.close();
         long endTimes = System.currentTimeMillis();
 
